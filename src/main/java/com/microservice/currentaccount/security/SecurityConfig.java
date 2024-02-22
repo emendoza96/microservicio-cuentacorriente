d6ea2434dc896f43b9c91c446f03b219dda8beac
emiliano <eemmiii96@gmail.com>
2024-02-22 19:46:17 -0300

    Remove unused authentication from SecurityConfig.java

--- src/main/java/com/microservice/currentaccount/security/SecurityConfig.java
+++ src/main/java/com/microservice/currentaccount/security/SecurityConfig.java
@@ -3,25 +3,18 @@ package com.microservice.currentaccount.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import com.microservice.currentaccount.security.filters.JwtAuthorizationFilter;
-import com.microservice.currentaccount.service.impl.UserDetailsServiceImpl;
 
 @Configuration
 @EnableMethodSecurity(prePostEnabled = true)
 public class SecurityConfig {
 
-    @Autowired
-    private UserDetailsServiceImpl userDetailsServiceImpl;
 
     @Autowired
     private JwtAuthorizationFilter authorizationFilter;
@@ -43,17 +36,4 @@ public class SecurityConfig {
             .build();
     }
 
-    @Bean
-    PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
-
-    @Bean
-    AuthenticationManager authenticationManager(HttpSecurity httpSecurity, PasswordEncoder passwordEncoder) throws Exception {
-        return httpSecurity.getSharedObject(AuthenticationManagerBuilder.class)
-            .userDetailsService(userDetailsServiceImpl)
-            .passwordEncoder(passwordEncoder)
-            .and()
-            .build();
-    }
 }
